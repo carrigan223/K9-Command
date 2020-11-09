0476935b1f5605de15f1e7c07aefcc1c09c7c38e
Andrew Carrigan <carrigan223@gmail.com>
2020-11-09 14:52:42 -0800

    implementing react bootstrap

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `K9-Obey`,
+    description: `San Diego puppies favorite spot for trainging and boarding`,
+    author: `Andrew Carrigan`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
