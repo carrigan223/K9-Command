b85a454cb61db70de0438afb251967fb51b07b7c
Andrew Carrigan <carrigan223@gmail.com>
2020-11-16 10:52:06 -0800

    Monday

--- src/pages/rates.js
+++ src/pages/rates.js
@@ -8,7 +8,7 @@ const SecondPage = () => (
   <Layout>
     <SEO title="Rates" />
     <h1>Hi from the Rates page</h1>
-    <p>Welcome to page 5</p>
+    <p>This is for rates</p>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
